83a7f024302d55defa80414fd9e386c5fb44a9eb
Shubham Jain <shubham9415jain@gmail.com>
2019-11-04 00:29:47 +0530

    json serve path changes

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/pharma-webapp"));
+app.use(express.static(__dirname + "/dist/pharmatask"));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/pharma-webapp/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/pharmatask/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
